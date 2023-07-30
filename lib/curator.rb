e38ded44411976b804873c9b3021338812fe061a
Will Weston <Williamweston81@gmail.com>
2023-07-30 15:11:42 -0500

    Weston(1) Complete Iteartion 3

--- lib/curator.rb
+++ lib/curator.rb
@@ -19,4 +19,36 @@ class Curator
       artist.id.to_s == id.to_s
     end
   end
+
+  def return_list
+    return_list = {}
+
+    @artists.each do |artist|
+      photos = @photographs.select { |photo| photo.artist_id.to_s == artist.id.to_s }
+      return_list[artist] = photos
+      end
+    return_list
+  end
+
+  def more_than_one_photo
+    return_list = Hash.new(0)
+
+    @photographs.each do |photo|
+      return_list[photo.artist_id.to_s] += 1
+    end
+
+    more_than_one_photo = []
+
+    @artists.each do |artist|
+      more_than_one_photo  << artist.name if return_list[artist.id.to_s] > 1
+    end
+    more_than_one_photo
+  end
+
+  def from_country(country)
+    @photographs.select do |photo|
+      @artist = find_artist_by_id(photo.artist_id)
+      @artist.country == country
+    end
+  end
 end
--- spec/curator_spec.rb
+++ spec/curator_spec.rb
@@ -10,13 +10,15 @@ RSpec.describe Curator do
       name: "Rue Mouffetard, Paris (Boy with Bottles)",      
       artist_id: "1",      
       year: "1954"      
-      })        
+      })       
+
     @photo_2 = Photograph.new({
       id: "2",      
       name: "Moonrise, Hernandez",      
       artist_id: "2",      
       year: "1941"      
-      })     
+      })
+
     @artist_1 = Artist.new({
         id: "1",      
         name: "Henri Cartier-Bresson",      
@@ -24,13 +26,14 @@ RSpec.describe Curator do
         died: "2004",      
         country: "France"      
       })
+
     @artist_2 = Artist.new({
       id: "2",      
       name: "Ansel Adams",      
       born: "1902",      
       died: "1984",      
       country: "United States"      
-  })      
+      })      
   end
 
   it "exists with 0 photogrpahs" do
@@ -55,5 +58,47 @@ RSpec.describe Curator do
 
     expect(@curator.artists).to eq([@artist_1, @artist_2])
     expect(@curator.find_artist_by_id("1")).to eq(@artist_1)
+  end
+
+  it 'return a list of all artists and their photographs' do
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    expected_result = {
+      @artist_1 => [@photo_1],
+      @artist_2 => [@photo_2]
+    }
+
+    expect(@curator.return_list).to be_a Hash
+    expect(@curator.return_list).to eq(expected_result)
+  end
+
+  it 'return list of artists who have more than 1 photo' do
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    # require 'pry' ; binding.pry
+
+    expect(@curator.more_than_one_photo).to be_a Array
+    expect(@curator.more_than_one_photo).to eq([])
+  end
+
+
+  it 'return a list of `Photograph`s that were taken by a photographer from that country.' do
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    # require 'pry' ; binding.pry
+    expected_result = [@photo_1]
+    # require 'pry' ; binding.pry
+
+    expect(@curator.from_country("France")).to be_a Array
+    expect(@curator.from_country("France")).to eq(expected_result)
   end
 end
